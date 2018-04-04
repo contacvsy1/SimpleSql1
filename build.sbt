26e8dd4bca12f73fe6a9bb6a82aa2ed0b21f622a
vijay <contact.vsy@gmail.com>
2018-04-04 00:27:35 -0700

    initial spark sql

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "SimpleSql1"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
+
+fork in run := true
---
+++ src/main/scala/SimpleSql1.scala
@@ -0,0 +1,73 @@
+/* SimpleSql1.scala */
+import org.apache.spark.sql.SparkSession
+import java.io.File
+import java.io.PrintWriter
+// import spark.implicits._
+
+object SimpleSql1 {
+  def main(args: Array[String]) {
+    val spark = SparkSession.builder.appName("Simple Application").config("spark.master", "local").getOrCreate()
+
+    import spark.implicits._
+
+    val df = spark.read.json("/home/vijay/Downloads/spark-2.2.0-bin-hadoop2.7/examples/src/main/resources/people.json")
+
+    df.printSchema()
+    // root
+    // |-- age: long (nullable = true)
+    // |-- name: string (nullable = true)
+
+    // Select only the "name" column
+    df.select("name").show()
+    // +-------+
+    // |   name|
+    // +-------+
+    // |Michael|
+    // |   Andy|
+    // | Justin|
+    // +-------+
+
+    // Select everybody, but increment the age by 1
+    df.select($"name", $"age" + 1).show()
+    // +-------+---------+
+    // |   name|(age + 1)|
+    // +-------+---------+
+    // |Michael|     null|
+    // |   Andy|       31|
+    // | Justin|       20|
+    // +-------+---------+
+
+    // Select people older than 21
+    df.filter($"age" > 21).show()
+    // +---+----+
+    // |age|name|
+    // +---+----+
+    // | 30|Andy|
+    // +---+----+
+
+    // Count people by age
+    df.groupBy("age").count().show()
+    // +----+-----+
+    // | age|count|
+    // +----+-----+
+    // |  19|    1|
+    // |null|    1|
+    // |  30|    1|
+    // +----+-----+
+
+
+    // Register the DataFrame as a SQL temporary view
+    df.createOrReplaceTempView("people")
+
+    val sqlDF = spark.sql("SELECT * FROM people")
+    sqlDF.show()
+    // +----+-------+
+    // | age|   name|
+    // +----+-------+
+    // |null|Michael|
+    // |  30|   Andy|
+    // |  19| Justin|
+    // +----+-------+
+
+  }
+}
